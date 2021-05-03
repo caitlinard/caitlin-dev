5daeb17f931321c531ae4ef270296224f6be92fd
Caitlin Ard <caitlin.ard@gmail.com>
2021-05-03 07:00:22 -0700

    working through next.js tutorial

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello world!</p>
+        <p>Some content will go here</p>
+      </section>
+    </Layout>
+  );
+}
